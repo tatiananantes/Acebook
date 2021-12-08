df86d47d670b11c6e817e7e382f1529c3d06530a
Adam Harbisher <aharbisher@gmail.com>
2021-12-08 12:04:18 +0000

    Created comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_111415) do
+ActiveRecord::Schema.define(version: 2021_12_08_120300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_12_08_111415) do
     t.string "user"
   end
 
+  add_foreign_key "comments", "posts"
 end
