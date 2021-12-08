dbb8e6af296abdb663cc69c8918742d1e808b471
Tatiana Nantes <tatiananantes@gmail.com>
2021-12-08 16:50:21 +0000

    user can sign up - add a sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to users_url
+
+    # if @user.save
+    # else
+    #   render :new
+    # end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+
 end
