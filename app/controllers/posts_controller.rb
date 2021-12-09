dc13d740bb29e6294dd6fe685f5ad1408aa81dbd
Adam Harbisher <aharbisher@gmail.com>
2021-12-09 16:52:30 +0000

    get user from session for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,8 @@ class PostsController < ApplicationController
 
     def create
       @user = User.find_by_id(session[:user_id])
-      # @post = @user.posts.create(params[:post])
-      respond_with Post.create(post_params.merge(user_id: @user.id))
-      # @post.save
-      redirect_to @post
+      Post.create(post_params.merge(user_id: @user.id))
+      redirect_to posts_path
     end
 
   def index
