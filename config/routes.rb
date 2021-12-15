1b9c1093a821dcf150caefab683b43f73f1d4d40
willjshark <willjshark@gmial.com>
2021-12-15 11:08:26 +0000

    Home page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
- # get 'likes/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "users#new"
   resources :users
   resources :posts do
     resources :comments, :likes 
